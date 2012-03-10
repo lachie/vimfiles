6f65bd0280f64cd508e1dc844a57c44cecce2359
Lachie Cox <lachiec@gmail.com>
2012-03-10 18:50:03 +1100

    Add powerline patched inconsolata

--- install.sh
+++ install.sh
@@ -7,3 +7,13 @@ fi
 
 ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/gvimrc ~/.gvimrc
+
+# mac only
+open fonts/Inconsolata-dz-Powerline.otf
+
+# XXX not working for me
+#vim -N -u ~/.vimrc -es - <<-VIM
+#BundleInstall!
+#BundleClean
+#q
+#VIM
